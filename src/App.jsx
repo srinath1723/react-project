52ee9c59e3533e24dac34867091bd72efeb311f3
srinath1723 <srinathsp890.com>
2024-05-22 13:06:56 +0530

    multiple hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+const Hello=()=>{
+  return <h1>Hello, World!</h1>
+}
+
+
 //component:App
 const App=()=>{
-      return <h1>Hello, World!</h1>
+      return (
+        <div>
+          <Hello />
+          <Hello />
+          <Hello />
+        </div>
+      )
 }
 export default App;
