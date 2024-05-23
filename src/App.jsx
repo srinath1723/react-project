e432dfbfbd4ca2213dc500fcda43e796f31cb144
srinath1723 <srinathsp890.com>
2024-05-23 12:50:45 +0530

    Handling Events type-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
-import Todo from "./components/Todo"
 const App = () => {
-  const todos=[
-    {id:1,title:'Buy butter',complete:false},
-    {id:2,title:'Buy milk',complete:true},
-    {id:3,title:'Buy bread',complete:false}
-  ]
+  const handleclick=()=>{
+    console.log('button click');
+  }
   return (
     <div>
-      <h1>Todo List</h1>
-      {
-      todos.map(todo =>
-      <Todo 
-      key={todos.id}
-      todo={todo}
-      />
-      )}
+      <h1>React App</h1>
+     <button onClick={handleclick}>Click me!</button>  
+     
       
     </div>
   )
