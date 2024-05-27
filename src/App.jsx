62cef14b3d4cad72611250b5cb691d6be1a727a5
srinath1723 <srinathsp890.com>
2024-05-27 12:46:21 +0530

    state and props

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,22 @@
 
-import { useEffect } from 'react';
-import { useState } from 'react';
 
+import { useState } from 'react';
+import Counter from './components/Counter';
 const App = () => {
 let[count,setCount]=useState(0);
-// runs when the component is mounted 
-// And whenever the state changes (any state within the component)
-useEffect(()=>{
-console.log("component mounted")
-},[]);
+
+
+
 const hangleIncrement=()=>{
   setCount(count+1);
 }
-const hangleDecrement=()=>{
-  if(count>0){
-  setCount(count-1);
-  }
-}
-const hangleReset=()=>{
-  setCount(0);
-}
+
   return (
     <div>
-      <h1>Count:{count }</h1>
+      <Counter
+      count={count} />
       <button onClick={hangleIncrement}>Increament</button>
-      <button onClick={hangleDecrement}>Decrement</button>
-      <button onClick={hangleReset}>Reset</button>
+      
     </div>
   )
 }
