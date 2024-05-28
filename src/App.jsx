afdecff5495169881b3bdeb817bb25529b89d207
srinath1723 <srinathsp890.com>
2024-05-28 12:55:49 +0530

    react-router-dom ---using components

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
 
 //Create a router
 const router=createBrowserRouter([
 {
       path:"/",
-      element:<h1>Hello,World!!</h1>
+      element:<Home />
 },{
-      path:"/about",
-      element:<h1>About us</h1>
-},{
-      path:"/contact",
-      element:<h1>Contact Us</h1>
-},{
-      path:"/services",
-      element:<h1>Our Services</h1>
-},{
-      path:"/products",
-      element:<h1>Our Products</h1>
+      path:"/register",
+      element:<Register />
 }
 ]);
 
---
+++ src/pages/Register.jsx
@@ -0,0 +1,11 @@
+
+
+const Register = () => {
+  return (
+    <div>
+      <h1>Register1</h1>
+    </div>
+  )
+}
+
+export default Register
