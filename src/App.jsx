703be26653e31575b13debeb6dc65f1d3d48805b
srinath1723 <srinathsp890.com>
2024-06-03 11:25:39 +0530

    useRef hook demonstration -- changing values re-rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { useRef } from "react";
 
 const App=()=>{
+      const [state,setState]=useState(0);
       //create a reference using useRef hook
       const inputRef=useRef(null);
       
@@ -9,6 +11,13 @@ const App=()=>{
            inputRef.current.focus();
            
       }
+      const hadleChange=()=>{
+            //focus the input element here
+            inputRef.current.value="HEllo world";// this will not trigger re-render
+            setState(state+1);//this will trigger re-render
+            
+       }
+       console.log("rendering component");
      
      
       return (
@@ -18,7 +27,7 @@ const App=()=>{
       ref={inputRef}
       />
       <button onClick={hadleClick}>Focus Inputs</button>
-      
+      <button onClick={hadleChange}>Change Value</button>
       </>
 )
 }
