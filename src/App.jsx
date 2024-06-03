3d69d098f4729efe3cb54146a921c6b886dded43
srinath1723 <srinathsp890.com>
2024-06-03 12:44:41 +0530

    useReducer -using state and dispatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
+import { useReducer } from "react"
+import { countReducer, initialState } from "./reducers/counterReducer"
 
-import { useState } from "react";
-import { useRef } from "react";
 
-const App=()=>{
-      const countRef=useRef(0);
-      const [state,setState]=useState(0);
-     const handleRefIncrement=()=>{
-      countRef.current +=1;
-      console.log(countRef.current);
-     }
-     const handleStateCount=()=>{
-      setState(state+1);
-    
-     }
-     console.log("component render");
-      return (
-      <>
-      <h1>State Count: {state}</h1>
-      <h1>Ref Count: {countRef.current}</h1>
-      <button onClick={handleRefIncrement}>Increment Ref Count</button>
-      <button onClick={handleStateCount}>Increment State Count</button>
-      </>
-)
+const App = () => {
+   const[state,dispatch] =  useReducer(countReducer,initialState)
+   const handleIncrement=()=>{
+      dispatch({type:"Increment"});
+   }
+   const handleDecrement=()=>{
+      dispatch({type:"Decrement"});
+   }
+  return (
+      <div>
+    <h1>Count:{state.count}</h1>
+    <button onClick={handleIncrement}>Increment</button>
+    <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  )
 }
-export default App;
+
+export default App
---
+++ src/reducers/counterReducer.js
@@ -0,0 +1,17 @@
+export const initialState={
+    count:0
+}
+export const  countReducer=(state,action)=>{
+   
+    switch(action.type){
+        case "Increment":
+           return {count: state.count+1}
+        case "Decrement":
+            return {count: state.count-1}
+        default:
+            return state;
+             
+
+    }
+
+}
