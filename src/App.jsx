db2e41c22650b0b482896785870d5d9b7a7f9b52
srinath1723 <srinathsp890.com>
2024-06-03 12:03:41 +0530

    useRef hook  variables+rendering in state situation

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
+
 import { useState } from "react";
 import { useRef } from "react";
 
 const App=()=>{
+      const countRef=useRef(0);
       const [state,setState]=useState(0);
-      //create a reference using useRef hook
-      const inputRef=useRef(null);
-      
-      const hadleClick=()=>{
-           //focus the input element here
-           inputRef.current.focus();
-           
-      }
-      const hadleChange=()=>{
-            //focus the input element here
-            inputRef.current.value="HEllo world";// this will not trigger re-render
-            setState(state+1);//this will trigger re-render
-            
-       }
-       console.log("rendering component");
-     
-     
+     const handleRefIncrement=()=>{
+      countRef.current +=1;
+      console.log(countRef.current);
+     }
+     const handleStateCount=()=>{
+      setState(state+1);
+    
+     }
+     console.log("component render");
       return (
       <>
-      <input 
-      type="text"
-      ref={inputRef}
-      />
-      <button onClick={hadleClick}>Focus Inputs</button>
-      <button onClick={hadleChange}>Change Value</button>
+      <h1>State Count: {state}</h1>
+      <h1>Ref Count: {countRef.current}</h1>
+      <button onClick={handleRefIncrement}>Increment Ref Count</button>
+      <button onClick={handleStateCount}>Increment State Count</button>
       </>
 )
 }
